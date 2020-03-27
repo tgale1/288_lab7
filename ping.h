4d07483bce86a8f4297688000dade87dc9a7e8e2
tgale1 <62774334+tgale1@users.noreply.github.com>
2020-03-27 16:33:54 -0500

    Update ping.h

--- ping.h
+++ ping.h
@@ -8,11 +8,11 @@
 #ifndef PING_H_
 #define PING_H_
 
-
-void ping_init (void); //initialize the timer
-void ping_trigger (void); //activate the PING sensor
-float ping_getDistance (void); //calculate distance
-void timer_handler(void);
+void ping_init_timer (void); //Initializes the timer
+void ping_send_pulse (void); //Sends the initial pulse
+int ping_read (void); //Reads the data incoming from the ping sensor
+float ping_getDistance (void); //calculate distance from the data from the ping sensor
+void timer_handler(void); //Handles interrupts in the timer
 
 
 #endif /* PING_H_ */
